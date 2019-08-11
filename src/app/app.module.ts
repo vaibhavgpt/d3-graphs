63df27e2b208ee773f274e25c85081667fea81bc
vaibhav <simply.vaibhavgupta@gmail.com>
2019-08-11 17:33:59 +0530

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CardLinkComponent } from './card-link/card-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    CardLinkComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
